d291023111b9bbc3c303b0f9fca85e310913e0b4
Gabrielanavesg <gabriela.goncalves3@fatec.sp.gov.br>
2022-12-01 22:42:32 +0000

    sincronzar Jessica

--- cursos.py
+++ cursos.py
@@ -14,4 +14,19 @@ cursos = [
 def home():
     return render_template('home.html', cursos=cursos)
 
+@app.route('/create', methods=['POST'])
+def create():
+    nomecurso = request.form['nomecurso']
+    areacurso = request.form['areacurso']
+    curso = {'curso': nomecurso, 'area': areacurso}
+    cursos.append(curso)
+    return render_template('home.html', cursos=cursos)
+
+#@app.route('/Deletar<int:curso', methods['POST'])
+def deletar(curso):
+    curso = request.form('nomecurso') 
+    curso = request.form('areacurso')
+    cursos.remove(curso)
+    return render_template('home.html', cursos=cursos)   
+
 app.run(debug=True)
