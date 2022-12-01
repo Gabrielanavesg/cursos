b45f5e47977a886fc867d1b3ee7dc7d2b1bafb85
Jooshica <tekinharusher@gmail.com>
2022-12-01 13:35:39 +0000

    salvei

--- cursos.py
+++ cursos.py
@@ -1,1 +1,15 @@
-print('Hello')
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+cursos = [
+    {'curso': 'Administração', 'area': 'Humanas'},
+    {'curso': 'Direito', 'area': 'Humanas'}
+    {'curso': 'Arquitetura', 'area': 'Sociais'}
+    {'curso': 'Historia', 'area': 'Humanas'}
+    {'curso': 'Engenharia Civil', 'area': 'Exatas'}
+]
+
+@app.route('/')
+def home():
+    return render_template('home.html', cursos=cursos)
