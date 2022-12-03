631203608a8a517a121fa3f56a5b803bad940e1e
Jooshica <tekinharusher@gmail.com>
2022-12-03 18:37:58 +0000

    Deletar deu certo

--- cursos.py
+++ cursos.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+URL = "https://5000-jooshica-cursos-ju9gm0rfcss.ws-us77.gitpod.io"
+
 cursos = [
-    {'curso': 'Administração', 'area': 'Humanas'},
-    {'curso': 'Direito', 'area': 'Humanas'},
-    {'curso': 'Arquitetura', 'area': 'Sociais'},
-    {'curso': 'Historia', 'area': 'Humanas'},
-    {'curso': 'Engenharia Civil', 'area': 'Exatas'}
+    {'curso': 'Administração'},
+    {'curso': 'Direito'},
+    {'curso': 'Arquitetura'},
+    {'curso': 'História'},
+    {'curso': 'Engenharia Civil'}
 ]
 
 @app.route('/')
@@ -17,16 +19,21 @@ def home():
 @app.route('/create', methods=['POST'])
 def create():
     nomecurso = request.form['nomecurso']
-    areacurso = request.form['areacurso']
-    curso = {'curso': nomecurso, 'area': areacurso}
+    curso = {'curso': nomecurso}
     cursos.append(curso)
     return render_template('home.html', cursos=cursos)
 
-@app.route('/delete/<int:id>')
-def delete(id):
-    for curso in cursos:
-        id = i =+ 1
-    cursos.pop(id)
-    return render_template('home.html', cursos=cursos, id=id)
+@app.route('/delete')
+def delete():
+    return render_template('delete.html')
+
+@app.route('/deletar', methods=['POST'])
+def deletar():
+    removercurso = request.form['deletarcurso']
+    curso_removido = { "curso": removercurso}
+    cursos.remove(curso_removido)
+          
+    return redirect(URL)
+
 
 app.run(debug=True)
