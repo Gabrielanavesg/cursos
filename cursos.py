80770527eae163c9904d2d7173407ff4b041b4f5
Jooshica <tekinharusher@gmail.com>
2022-12-04 15:04:45 +0000

    Tabela

--- cursos.py
+++ cursos.py
@@ -2,14 +2,21 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-URL = "https://5000-jooshica-cursos-ju9gm0rfcss.ws-us77.gitpod.io"
+URL = "https://5000-jooshica-cursos-tlbmw8g2pbj.ws-us77.gitpod.io"
 
 cursos = [
-    {'curso': 'Administração'},
-    {'curso': 'Direito'},
-    {'curso': 'Arquitetura'},
-    {'curso': 'História'},
-    {'curso': 'Engenharia Civil'}
+    {'curso': 'Administração', 'area': 'Humanas'},
+    {'curso': 'Direito', 'area': 'Humanas'},
+    {'curso': 'Arquitetura', 'area': 'Sociais'},
+    {'curso': 'História', 'area': 'Humanas'},
+    {'curso': 'Engenharia Civil', 'area': 'Exatas'},
+    {'curso': 'Publicidade', 'area': 'Humanas'},
+    {'curso': 'Medicina', 'area': 'Biológicas'},
+    {'curso': 'Nutrição', 'area': 'Biológicas'},
+    {'curso': 'Educação Física', 'area': 'Biológicas'},
+    {'curso': 'Moda', 'area': 'Humanas'},
+    {'curso': 'Física', 'area': 'Exatas'},
+    {'curso': 'Química', 'area': 'Biológicas'}
 ]
 
 @app.route('/')
@@ -19,7 +26,8 @@ def home():
 @app.route('/create', methods=['POST'])
 def create():
     nomecurso = request.form['nomecurso']
-    curso = {'curso': nomecurso}
+    areacurso = request.form['areacurso']
+    curso = {'curso': nomecurso, 'area': areacurso}
     cursos.append(curso)
     return render_template('home.html', cursos=cursos)
 
@@ -30,12 +38,13 @@ def delete():
 @app.route('/deletar', methods=['POST'])
 def deletar():
     removercurso = request.form['deletarcurso']
-    curso_removido = { "curso": removercurso}
+    removerarea = request.form['deletararea']        
+    curso_removido = { 'curso': removercurso, 'area': removerarea}
     if curso_removido in cursos:
         cursos.remove(curso_removido)
     else:
         return render_template('cursonaoencontrado.html')
-          
+
     return redirect(URL)
 
 
